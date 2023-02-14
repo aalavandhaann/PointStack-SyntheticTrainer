72b393fdd71328caa0cedea0ebaec9864ccb7c8c
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2023-02-14 18:30:17 -0500

    Updates
    - SBATCH Script
    - Config YAML file

--- requestgpu.sh
+++ requestgpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-rnoumeir
-#SBATCH --gres=gpu:v100:1       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=24  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --gres=gpu:5       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=15  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=01:00:00     # DD-HH:MM:SS
+#SBATCH --time=00:30:00     # DD-HH:MM:SS
 #SBATCH --mail-user=srinivasan.ramachandran.1@ens.etsmtl.ca
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ SOURCEDIR=~/Workspace/TensorFlowEnvironment/projects/PointStack-SyntheticTrainer
 # Prepare virtualenv
 # source ~/Workspace/TensorFlowEnvironment/bin/activate
 source ~/Workspace/TensorFlowEnvironment39/bin/activate
-tensorboard --logdir=./logs --host 0.0.0.0 --load_fast false & python $SOURCEDIR/train.py --cfg_file cfgs/syntheticpartnormal/syntheticpartnormal.yaml --exp_name synthetic_shapenet --val_steps=3000
+tensorboard --logdir=./logs --host 0.0.0.0 --load_fast false & python $SOURCEDIR/train.py --cfg_file cfgs/syntheticpartnormal/syntheticpartnormal.yaml --exp_name synthetic_shapenet
