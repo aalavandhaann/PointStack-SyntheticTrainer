324c1ac262d83e1103176120ba89f492c7929031
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2023-02-15 10:07:24 -0500

    Changes to requesgpy.sh

--- requestgpu.sh
+++ requestgpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:v100:4       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=20  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=00:30:00     # DD-HH:MM:SS
+#SBATCH --time=03-00:30:00     # DD-HH:MM:SS
 #SBATCH --mail-user=srinivasan.ramachandran.1@ens.etsmtl.ca
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ SOURCEDIR=~/Workspace/TensorFlowEnvironment/projects/PointStack-SyntheticTrainer
 # Prepare virtualenv
 # source ~/Workspace/TensorFlowEnvironment/bin/activate
 source ~/Workspace/TensorFlowEnvironment39/bin/activate
-tensorboard --logdir=./logs --host 0.0.0.0 --load_fast false & python $SOURCEDIR/train.py --cfg_file cfgs/syntheticpartnormal/syntheticpartnormal.yaml --exp_name synthetic_shapenet --val_steps=1
+tensorboard --logdir=$SOURCEDIR/experiments/SyntheticPartNormal/synthetic_shapenet/tensorboard/ --host 0.0.0.0 --load_fast false & python $SOURCEDIR/train.py --cfg_file cfgs/syntheticpartnormal/syntheticpartnormal.yaml --exp_name synthetic_shapenet --val_steps=1
