314f1232adec2b2850bbf9201780837bb32b5e0e
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2023-02-15 18:37:02 -0800

    Updates
    - Changes with regards to Cedar cluster

--- requestgpu.sh
+++ requestgpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=def-rnoumeir
-#SBATCH --gres=gpu:v100:4       # Request GPU "generic resources"
+#SBATCH --gres=gpu:p100:4       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=20  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=03-00:30:00     # DD-HH:MM:SS
@@ -13,4 +13,4 @@ SOURCEDIR=~/Workspace/TensorFlowEnvironment/projects/PointStack-SyntheticTrainer
 # Prepare virtualenv
 # source ~/Workspace/TensorFlowEnvironment/bin/activate
 source ~/Workspace/TensorFlowEnvironment39/bin/activate
-tensorboard --logdir=$SOURCEDIR/experiments/SyntheticPartNormal/synthetic_shapenet/tensorboard/ --host 0.0.0.0 --load_fast false & python $SOURCEDIR/train.py --cfg_file cfgs/syntheticpartnormal/syntheticpartnormal.yaml --exp_name synthetic_shapenet --val_steps=1
+tensorboard --logdir=$SOURCEDIR/experiments/SyntheticPartNormal/synthetic_shapenet/tensorboard/ --host 0.0.0.0 --load_fast false & python $SOURCEDIR/train.py --cfg_file $SOURCEDIR/cfgs/syntheticpartnormal/syntheticpartnormal.yaml --exp_name synthetic_shapenet --val_steps=1
